c9cd9af71ef99726b9c41233b766a398e0a4864d
Ozan <ozanozdeniz@gmail.com>
2025-04-16 23:44:51 -0400

    NextAuth.js yapılandırması güncellendi

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,55 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
-import { db } from './db'
 import GoogleProvider from 'next-auth/providers/google'
+import { db } from './db'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db) as any,
   session: {
-    strategy: 'jwt',
+    strategy: 'jwt'
+  },
+  pages: {
+    signIn: '/auth/signin'
   },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
   ],
   callbacks: {
-    session: async ({ session, token }) => {
-      if (session?.user && token.sub) {
-        session.user.id = token.sub
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+        session.user.role = token.role
       }
+
       return session
     },
-  },
-  pages: {
-    signIn: '/auth/signin',
-  },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email
+        }
+      })
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+        role: dbUser.role
+      }
+    }
+  }
 } 
