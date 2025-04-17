9af0c0cc00fc8e3f4d1bb976f7422250713b78e6
Ozan <ozanozdeniz@gmail.com>
2025-04-16 23:14:48 -0400

    Prisma istemcisi yapılandırması güncellendi

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import db from "./db"
+import { db } from "./db"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db) as any,
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient()
+export const db = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-}
-
-export default prisma 
+  globalForPrisma.prisma = db
+} 
