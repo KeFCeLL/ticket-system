f073b587c3fcbad99853de5a14032d040b321744
Ozan <ozanozdeniz@gmail.com>
2025-04-16 23:16:28 -0400

    Prisma istemcisi yapılandırması güncellendi

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,8 +4,10 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClient()
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = db
-} 
+  globalForPrisma.prisma = prisma
+}
+
+export const db = prisma 
